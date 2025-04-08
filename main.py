f03381289d82fa91a774f8b6faae8a982930c3d5
aang <pitercafe25@gmail.com>
2025-04-08 21:53:33 +0300

    #150

--- main.py
+++ main.py
@@ -147,26 +147,26 @@ def callbackto(call):
   if call.data =='ПОСМОТРЕТЬ ЗАКАЗ':  
     # SORTED_BASKET = {}  # Создаем пустую корзину в виде словаря
     bot.send_message(call.message.chat.id , BASKET)
-    while True:
-        item = input("Введите название товара и количество через пробел (или 'стоп' для выхода): ")
-        if item.lower() == 'стоп':
-            break  # Завершаем ввод, если пользователь ввел 'стоп'
+    # while True:
+    #     item = input("Введите название товара и количество через пробел (или 'стоп' для выхода): ")
+    #     if item.lower() == 'стоп':
+    #         break  # Завершаем ввод, если пользователь ввел 'стоп'
     
-        parts = item.rsplit(" ", 1)
-        if len(parts) == 2 and parts[1].isdigit():
-            item_name, quantity = parts[0], int(parts[1])
-        else:
-            item_name, quantity = item, 1  # Если количество не указано, берем 1
+    #     parts = item.rsplit(" ", 1)
+    #     if len(parts) == 2 and parts[1].isdigit():
+    #         item_name, quantity = parts[0], int(parts[1])
+    #     else:
+    #         item_name, quantity = item, 1  # Если количество не указано, берем 1
     
-        if item_name in SORTED_BASKET:
-             SORTED_BASKET[item_name] += quantity # Увеличиваем количество, если товар уже есть в корзине
-             bot.send_message(call.message.chat_id,f"Товар '{item_name}' добавлен в корзину! Количество: {SORTED_BASKET[item_name]}") 
-        else:
-             SORTED_BASKET[item_name] = quantity
-             bot.send_message(call.message.chat_id,f"Товар '{item_name}' добавлен в корзину! Количество: {SORTED_BASKET[item_name]}")    
-    bot.send_message(call.message.chat_id("\nВаши товары в корзине:"))
-    for i, (product, quantity) in enumerate(SORTED_BASKET.items(), 1):
-        bot.send_message(call.message.chat_id (f"{i}. {product} - {quantity} шт."))  
+    #     if item_name in SORTED_BASKET:
+    #          SORTED_BASKET[item_name] += quantity # Увеличиваем количество, если товар уже есть в корзине
+    #          bot.send_message(call.message.chat_id,f"Товар '{item_name}' добавлен в корзину! Количество: {SORTED_BASKET[item_name]}") 
+    #     else:
+    #          SORTED_BASKET[item_name] = quantity
+    #          bot.send_message(call.message.chat_id,f"Товар '{item_name}' добавлен в корзину! Количество: {SORTED_BASKET[item_name]}")    
+    # bot.send_message(call.message.chat_id("\nВаши товары в корзине:"))
+    # for i, (product, quantity) in enumerate(SORTED_BASKET.items(), 1):
+    #     bot.send_message(call.message.chat_id (f"{i}. {product} - {quantity} шт."))  
 
     # for (key,value) in list(SORTED_BASKET.items()):
 
